999ca94f94878c851ae0d4b323feb4eefa11f6e6
karlosferreira <desenvolvimento.carlosferreira@gmail.com>
2025-02-11 11:12:21 -0300

    clean

--- src/index.js
+++ src/index.js
@@ -1,15 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { sendToVercelAnalytics } from './vitals';
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import App from "./App";
 
-reportWebVitals(sendToVercelAnalytics);
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
